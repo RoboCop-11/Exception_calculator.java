c71be2877c1317599c0f3f90572cbeb75f5ba99d
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:20:32 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+//Manan Khanna
+//AIML B3
+//23070126156
+
+package operations;
+
+public class Multiplication {
+    // Method to perform multiplication of two numbers
+    public static double multiply(double a, double b) {
+        return a * b; // Return the product of a and b
+    }
+}
