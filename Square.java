b5e38f037911876d2719c1b0cba9e49539e922f1
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:20:48 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+//Manan Khanna
+//AIML B3
+//23070126156
+
+package operations;
+
+public class Square {
+    // Method to calculate square of a number
+    public static double square(double a) {
+        return a * a; // Return square of input
+    }
+}
+
