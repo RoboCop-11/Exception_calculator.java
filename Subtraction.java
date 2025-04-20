f7946adfc9fe42c9e24799030e330036bcf7483e
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:21:10 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,12 @@
+//Manan Khanna
+//AIML B3
+//23070126156
+
+package operations;
+
+public class Subtraction {
+    // Method to perform subtraction of two numbers
+    public static double subtract(double a, double b) {
+        return a - b; // Return the result of a minus b
+    }
+}
