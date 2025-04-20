ccfa11f56fa010c6010e2c940b5457bc4d55dd81
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:20:10 +0530

    Add files via upload

---
+++ Cube.java
@@ -0,0 +1,11 @@
+//Manan Khanna
+//AIML B3
+//23070126156
+package operations;
+
+public class Cube {
+    // Method to calculate cube of a number
+    public static double cube(double a) {
+        return a * a * a; // Return cube of input
+    }
+}
