4798e3083a4f1d0dd3b82850bf7a71916266aa6f
RoboCop-11 <itsleooo20@gmail.com>
2025-04-21 00:19:59 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+//Manan Khanna
+//AIML B3
+//23070126156
+
+package operations; // Package declaration
+
+public class Addition {
+    // Method to perform addition of two numbers
+    public static double add(double a, double b) {
+        return a + b; // Return the sum of a and b
+    }
+}
